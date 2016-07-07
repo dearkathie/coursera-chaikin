a33db148ed213cfb3091d54853476b66d36853eb
Katja Vegner <katjavegner@gmail.com>
2016-07-07 23:50:03 +0300

    chaikin's solutions

---
+++ module 4/SpeakHello.js
@@ -0,0 +1,8 @@
+(function(window) {
+    var helloSpeaker = {};
+    var speakWord = "Hello";
+    helloSpeaker.speak = function() {
+        console.log(speakWord + " " + names[i]);
+    }
+    window.helloSpeaker = helloSpeaker;
+})(window);
